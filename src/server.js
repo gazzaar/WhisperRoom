f77584d6a98c99adae8b615e0fd7df84e830c2e4
gazzaar <gazzarDev@gmail.com>
2025-07-04 00:51:32 +0300

    Set up express-session with connect-pg

--- src/server.js
+++ src/server.js
@@ -5,6 +5,14 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import signUpRouter from './routes/signUp.js';
 import loginRouter from './routes/login.js';
+import pool from './models/pool.js';
+import connectPgSimple from 'connect-pg-simple';
+import session from 'express-session';
+const connectPg = connectPgSimple(session);
+const sessionStore = new connectPg({
+  pool: pool,
+  tableName: 'session',
+});
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,8 +25,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Setting up session
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 1 Day
+    },
+  }),
+);
+
 app.get('/', (req, res) => {
-  res.send('hello world');
+  if (req.session.viewCount) {
+    req.session.viewCount++;
+  } else {
+    req.session.viewCount = 1;
+  }
+  res.send(`hello world, you viewed our page ${req.session.viewCount} times!`);
+
   res.end();
 });
 
